6e071115fcaa4444e1f4d78b8fa51de531a5bb21
abdullah yalçın <a.ylcin47@gmail.com>
2023-03-07 14:36:37 +0300

    tetsts yenilendi

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("git test");
+// let sayı = ["1","2","3"];
+
+ 
+// console.log(sayı[1]);
+
+
+const car = ["abc" ,"asd","bmw"];
+
+document.getElementById("demo").innerHTML = car;
+
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// document.getElementById("demo").innerHTML = cars;
